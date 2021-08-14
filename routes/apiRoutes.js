4af4e69179e53f49bc73efc7ff3e6027f676fc89
Burak Aksu <sabburak26@gmail.com>
2021-08-13 21:59:14 -0400

    finished hw

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,5 @@
-const db = require("../db/db.json")
+const fs = require("fs");
+const {v4 : uuidv4} = require('uuid')
 
 
 //GET /api/notes should read the db.json file and return all saved notes as JSON.
@@ -7,5 +8,53 @@ const db = require("../db/db.json")
 
 
 module.exports = (app) => {
-  app.get("/api/notes",)
+  app.get("/api/notes", (req, res) => {
+    const data = fs.readFileSync("./db/db.json", "utf8");
+    const jsonData = JSON.parse(data);
+    
+    res.json(jsonData)
+  })
+  
+  app.post("/api/notes", (req, res) => {
+    const data = fs.readFileSync("./db/db.json", "utf8");
+    const jsonData = JSON.parse(data);
+    const userID = uuidv4();
+    req.body.id = userID;
+    jsonData.push(req.body);
+    const data11 = JSON.stringify(jsonData);
+    fs.writeFileSync("./db/db.json", data11);
+    res.json(jsonData);
+  })
+
+  app.delete('/api/notes/:id', (req, res) => {
+    const chosen = req.params.id;
+    const data = fs.readFileSync("./db/db.json", "utf8");
+    const jsonData = JSON.parse(data);
+    let arr1;
+    let arr2;
+    let arr3;
+    for (let i = 0; i<jsonData.length; i++) {
+      if (chosen === jsonData[i].id) {
+        switch(i) {
+          case 0:
+            jsonData.shift();
+            arr3 = jsonData;
+            break;
+          case jsonData.length -1:
+            jsonData.pop();
+            arr3 = jsonData;
+            break;
+          default:
+            arr1 = jsonData.slice(0, i);
+            arr2 = jsonData.slice(i+1);
+            arr3 = arr1.concat(arr2);
+        }
+        break;  
+      }
+    }
+    
+    const data11 = JSON.stringify(arr3);
+    fs.writeFileSync("./db/db.json", data11);
+    res.json(arr3);
+  })
 }
